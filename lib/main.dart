75f3010b2e6496f0e3567ace4410002fd25a15cd
Nelson Bryan <201913582B@utea.edu.pe>
2022-12-11 11:30:42 -0500

    button of create , delete , update and create2

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tasks/pages/homepage.dart';
 
-void main(List<String> args) async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
